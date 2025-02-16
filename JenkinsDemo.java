25fdfd5b1054f8f7e29ff9b20199c1f767632f7c
Shubham Mane <mane6shubham@gmail.com>
2025-02-16 14:31:23 +0530

    initial commit

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,6 @@
 public class JenkinsDemo{
 public static void main(String[] args){
  System.out.println("This is for Jenkins Checking purpose");
+ System.out.println("Hi Shubham this is again from jenkins");
 }
 }
